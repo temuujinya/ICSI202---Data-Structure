69ce6b9cc5dcaaaef4381882e2c71139181419a8
USOTT <y.usott@gmail.com>
2018-12-23 22:31:35 +0800

    insertion finished

--- exam_pre/insertion.cpp
+++ exam_pre/insertion.cpp
@@ -1,15 +1,31 @@
+#include <iostream>
 
+using namespace std;
+void printAll(int *arr, int length);
 
 int main(){
     int arr[] = {8,45,433,45,23,234,4354,35,4656,234,234,2,654};
     int length = sizeof(arr)/sizeof(*arr);
 
-    for(int i=0; i<length;i++){
+    for(int i=1; i<length;i++){
+        int temp =  arr[i], 
+                    j=i-1;
         
-        for(int j=i+1;j<length;j++){
-            
+        while(j>=0 && temp<arr[j]){
+            arr[j+1] = arr[j];
+            j-=1;
         }
 
+        arr[j+1] = temp;
     }
-    
+    printAll(arr, length);
+}
+
+
+void printAll(int *arr, int length){
+    if(length==0)
+        return;
+    printAll(arr,length-1);
+    //minus one cause array start from 0 but length 1
+    cout<<arr[length-1]<<" ";
 }
